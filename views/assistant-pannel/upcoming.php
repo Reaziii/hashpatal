46258b0a3dca902035c2f1590743a748c28b6426
Reaz Ahammed <baphonreaz@gmail.com>
2023-04-03 01:59:29 +0600

    adding title

--- views/assistant-pannel/upcoming.php
+++ views/assistant-pannel/upcoming.php
@@ -16,7 +16,7 @@ var_dump($msg);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Assistant pannel | upcoming</title>
     <?php include ROOT . "/includes.php"; ?>
 </head>
 
